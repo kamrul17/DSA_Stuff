a42b55fc864f0f8ea43367dfada0a6f820276350
kamrul17 <72572933+kamrul17@users.noreply.github.com>
2023-11-20 17:38:08 +0530

    k

--- hashing/hash.cpp
+++ hashing/hash.cpp
@@ -4,11 +4,16 @@ using namespace std;
 int main()
 {
 
-    int arr[5] = {1, 2, 3, 5, 4};
+    int arr[5] = {1, 2, 3, 4, 5};
     int hash[6] = {0};
     for (int i = 0; i < 5; i++)
     {
         hash[arr[i]] += 1;
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout << hash[i] << endl;
     }
 
     int q = 5;
